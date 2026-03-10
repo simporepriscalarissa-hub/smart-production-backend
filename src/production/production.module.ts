3539671934f9147818988a1e844bf03883577158
Larissa Prisca Simpore <simporepriscalarissa@gmail.com>
2026-03-10 21:15:20 +0100

    Fix app.module - ajout EventsModule

--- src/production/production.module.ts
+++ src/production/production.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductionService } from './production.service';
 import { ProductionController } from './production.controller';
 import { Production } from './entities/production.entity';
+import { EventsModule } from '../events/events.module'; // ← ajoute cet import
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Production])],
+  imports: [
+    TypeOrmModule.forFeature([Production]),
+    EventsModule, // ← ajoute ça
+  ],
   controllers: [ProductionController],
   providers: [ProductionService],
 })
